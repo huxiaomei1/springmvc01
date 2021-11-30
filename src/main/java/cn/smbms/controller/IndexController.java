430c7accbda1747e406087a2d47afeeb434362c5
huxiaomei1 <1410067616@qq.com>
2021-11-30 11:53:58 +0800

    Initial commit

---
+++ src/main/java/cn/smbms/controller/IndexController.java
@@ -0,0 +1,20 @@
+package cn.smbms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@Controller
+public class IndexController extends AbstractController {
+    @Override
+    @RequestMapping("index.html")
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        System.out.println("你好，哈哈哈");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: huxiaomei
+  Date: 2021/11/30
+  Time: 8:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1> hello 你好！</h1>
+</body>
+</html>
